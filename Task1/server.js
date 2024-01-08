0b5296ccfa2dda59457580f3107bf42c41be5031
Prohar 75 <egor.prohorenko@gmail.com>
2024-01-08 14:38:01 +0300

    changing main code

--- Task1/server.js
+++ Task1/server.js
@@ -1,27 +1,13 @@
-const express = require("express")
-const fs = require("fs")
-const app = express()
-const port = 3000;
-const filePath = "api/greetings.json"
+import express from "express";
+const app = express();
+const PORT = 3000;
+const URL = "/api/greetings";
 
-let jsonName
+app.get(URL, function (req, res) {
+  const name = req.query.Id;
 
-fs.readFile(filePath, 'utf-8', (err, data) => {
-  if (err) {
-    console.error('Error: ', err);
-    return;
-  }
-  jsonName = JSON.parse(data);
+  if (name) res.send(`hello ${name}`);
+  else console.error("there is no value");
 });
-app.set("view engine", "ejs")
 
-app.get("/", (req, res) => {
-    res.render('index', {text: jsonName.name})
-
-    if (!jsonName) {
-        res.status(400).json("Error: there is no value");
-        return;
-    }
-})
-
-app.listen(port)
+app.listen(PORT);
