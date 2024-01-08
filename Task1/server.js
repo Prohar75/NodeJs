df10cab5114e1b7d76a4a4d2c872016845593bb7
Prohar 75 <egor.prohorenko@gmail.com>
2024-01-08 14:40:15 +0300

    commenting local host link

--- Task1/server.js
+++ Task1/server.js
@@ -3,6 +3,8 @@ const app = express();
 const PORT = 3000;
 const URL = "/api/greetings";
 
+//http://localhost:3000/api/greetings?Id=Egor
+
 app.get(URL, function (req, res) {
   const name = req.query.Id;
 
