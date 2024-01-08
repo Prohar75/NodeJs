9f54d142421185e549e9d3edc3c3bb0ff7a37cca
Prohar 75 <egor.prohorenko@gmail.com>
2024-01-08 14:49:35 +0300

    added try catch

--- Task1/server.js
+++ Task1/server.js
@@ -6,10 +6,14 @@ const URL = "/api/greetings";
 //http://localhost:3000/api/greetings?Id=Egor
 
 app.get(URL, function (req, res) {
-  const name = req.query.Id;
+  try {
+    const name = req.query.Id;
 
-  if (name) res.send(`hello ${name}`);
-  else console.error("there is no value");
+    if (name) res.send(`hello ${name}`);
+    else throw new Error("there is no value");
+  } catch (error) {
+    next(error);
+  }
 });
 
 app.listen(PORT);
