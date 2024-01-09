9233efa13c1cdc554bbe0dad255ba4916f6aea92
Prohar 75 <egor.prohorenko@gmail.com>
2024-01-09 17:37:02 +0300

    little code change

--- Task1/server.js
+++ Task1/server.js
@@ -3,14 +3,17 @@ const app = express();
 const PORT = 3000;
 const URL = "/api/greetings";
 
-//http://localhost:3000/api/greetings?Id=Egor
+//http://localhost:3000/api/greetings?Name=Egor
 
 app.get(URL, function (req, res) {
   try {
-    const name = req.query.Id;
+    const name = req.query.Name;
 
-    if (name) res.send(`hello ${name}`);
-    else throw new Error("there is no value");
+    if (name) {
+      res.send(`hello ${name}`);
+    } else {
+      throw new Error("there is no value");
+    }
   } catch (error) {
     next(error);
   }
