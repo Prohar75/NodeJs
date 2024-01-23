126a8fffb71e5ba9115bc15ae6e3e5c3548e06a8
Prohar75 <egor.prohorenko@gmail.com>
2024-01-22 23:30:02 -0800

    add mongo db and greate func

--- server.js
+++ server.js
@@ -1,37 +1,71 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongodb from "mongodb";
 const app = express();
 const PORT = 3001;
-const URL = "/api/greetings";
+const dirname = "D:/-git-repositories/NodeJs/";
+const connectionString =
+  "mongodb+srv://Prohar:PEA16april2005@cluster0.bosgtkc.mongodb.net/?retryWrites=true&w=majority";
 
-const __dirname = "D:/.git-repositories/NodeJs/";
+const URL = {
+  GREETINGS: "/api/greetings",
+  NOTES: "/api/notes",
+};
 
-//http://localhost:3001/api/greetings?name=Egor
-app.use(bodyParser.urlencoded({ extended: true }));
+mongodb.MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to Database");
 
-app.get(URL, function (req, res) {
-  try {
-    const name = req.query.name;
+    const db = client.db("NodeJs-Task");
+    const quotesCollection = db.collection("quotes");
 
-    if (name) {
-      res.send(`hello ${name}`);
-    } else {
-      throw new Error("there is no value");
-    }
-  } catch (error) {
-    //next(error);
-  }
-});
+    app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+    app.use(bodyParser.urlencoded({ extended: true }));
+    
+    app.get(URL.GREETINGS, function (req, res) {
+      try {
+        const name = req.query.name;
 
-app.post("/quotes", (req, res) => {
-  console.log(req.body);
-});
+        if (name) {
+          res.send(`hello ${name}`);
+        } else {
+          throw new Error("there is no value");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    });
 
-console.clear();
+    app.get("/", (req, res) => {
+      res.sendFile(dirname + "/index.html");
+    });
+
+    app.get(URL.NOTES, (req, res) => {
+      res.sendFile(dirname + "/index.html");
+    });
+
+    app.get(URL.NOTES, (req, res) => {
+      quotesCollection
+        .find()
+        .toArray()
+        .then(results => {
+          res.render('index.ejs', { quotes: results })
+        })
+        .catch(/* ... */);
+    });
+
+    app.post(URL.NOTES, (req, res) => {
+      res.sendFile(dirname + "/index.html");
+      quotesCollection
+        .insertOne(req.body)
+        .then((result) => {
+          console.log(result);
+        })
+        .catch((error) => console.error(error));
+    });
+  })
+  .catch((error) => console.error(error));
 
 console.log(
   `\n--------------------
